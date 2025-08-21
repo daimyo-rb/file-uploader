392feaffd7ae2586f0bf44157b47ad47baae9d07
daimyo-rb <daimyo@roshibros.com>
2025-08-20 21:22:48 -0500

    Require auth to create and delete folders

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -1,13 +1,14 @@
+const { isAuth } = require('../middleware/authMiddleware');
 const { Router } = require('express');
 const folderController = require('../controllers/folderController');
 
 const folderRouter = Router();
 
-folderRouter.get('/create-folder', folderController.getCreateFolder);
-folderRouter.post('/create-folder', folderController.postCreateFolder);
-folderRouter.get('/:folderId/edit', folderController.getUpdateFolder);
-folderRouter.post('/:folderId/edit', folderController.postUpdateFolder);
-folderRouter.get('/:folderId/delete', folderController.getDeleteFolder);
+folderRouter.get('/create-folder', isAuth, folderController.getCreateFolder);
+folderRouter.post('/create-folder', isAuth, folderController.postCreateFolder);
+folderRouter.get('/:folderId/edit', isAuth, folderController.getUpdateFolder);
+folderRouter.post('/:folderId/edit', isAuth, folderController.postUpdateFolder);
+folderRouter.get('/:folderId/delete', isAuth, folderController.getDeleteFolder);
 folderRouter.get('/:folderId', folderController.getFolder);
 
 module.exports = folderRouter;
